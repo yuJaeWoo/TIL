be518f2cecb0a15a2340a2beac7821e69bfa6db5
yuJaeWoo <67939789+yuJaeWoo@users.noreply.github.com>
2021-02-19 12:17:49 +0900

    Update prac.js

--- prac.js
+++ prac.js
@@ -635,3 +635,65 @@ app.listen(port, () => {
 
 
 // 9:28 view(컴포넌트)에서 어떤 이벤트가 발생하면 store의 값을 수정하는 로직을 실행한다고 합시다 9:28 그렇게 하기 위해서는 그 컴포넌트랑 store가 연결되어 있어야 돼요 9:28 그래서 쓰는 함수가 connect죠 9:29 자 그런데 여기서 발생한 이벤트가 store의 값을 수정해야하는 이벤트니까 9:29 action을 전달해줘야해요 9:29 dispatch가 필요해요 9:30 그래서 connect(null, mapDispatchToProps)(컴포넌트) 9:30 해당 컴포넌트에 디스패치를 연결해줘요 9:31 디스패치를 연결해줬다는건 즉 store와 연결해줬다는 뜻이에요 9:31 디스패치가 리듀서 액션을 전달할거니까요 9:32 리듀서는 액션을 받아서 스토어를 업데이트해요 9:33 여기서 뷰(컴포넌트)가 업데이트 되어야하는데 어떤 뷰가 업데이트 되느냐 9:33 스토어랑 연결된 컴포넌트중에 스토어에서 값을 꺼내 쓰는 애들 9:33 그 중에서 값이 변경된 애들 9:33 아직 값을 꺼내쓰는 연결은 위에서 언급된적이 없어요 9:34 값을 꺼내 쓰는건 connect(mapStateToProps)(컴포넌트2) 9:34 이렇게 연결을 해요 새 항목 9:34 그럼 컴포넌트2는 스토어에서 값을 꺼내 쓸 수가 있어요 9:35 그럼 아까전으로 돌아가서 스토어가 업데이트 될때 9:35 컴포넌트 2에서 쓰고있는 상태 값이 변경이 됐으면 9:35 컴포넌트 2는 다시 렌더링이 돼요 
+
+
+{
+  "drinks"
+:
+  [{
+    "idDrink": "11007",
+    "strDrink": "Margarita",
+    "strDrinkAlternate": null,
+    "strDrinkES": null,
+    "strDrinkDE": null,
+    "strDrinkFR": null,
+    "strDrinkZH-HANS": null,
+    "strDrinkZH-HANT": null,
+    "strTags": "IBA,ContemporaryClassic",
+    "strVideo": null,
+    "strCategory": "Ordinary Drink",
+    "strIBA": "Contemporary Classics",
+    "strAlcoholic": "Alcoholic",
+    "strGlass": "Cocktail glass",
+    "strInstructions": "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.",
+    "strInstructionsES": null,
+    "strInstructionsDE": "Reiben Sie den Rand des Glases mit der Limettenscheibe, damit das Salz daran haftet. Achten Sie darauf, dass nur der \u00e4u\u00dfere Rand angefeuchtet wird und streuen Sie das Salz darauf. Das Salz sollte sich auf den Lippen des Genie\u00dfers befinden und niemals in den Cocktail einmischen. Die anderen Zutaten mit Eis sch\u00fctteln und vorsichtig in das Glas geben.",
+    "strInstructionsFR": null,
+    "strInstructionsZH-HANS": null,
+    "strInstructionsZH-HANT": null,
+    "strDrinkThumb": "https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg",
+    "strIngredient1": "Tequila",
+    "strIngredient2": "Triple sec",
+    "strIngredient3": "Lime juice",
+    "strIngredient4": "Salt",
+    "strIngredient5": null,
+    "strIngredient6": null,
+    "strIngredient7": null,
+    "strIngredient8": null,
+    "strIngredient9": null,
+    "strIngredient10": null,
+    "strIngredient11": null,
+    "strIngredient12": null,
+    "strIngredient13": null,
+    "strIngredient14": null,
+    "strIngredient15": null,
+    "strMeasure1": "1 1\/2 oz ",
+    "strMeasure2": "1\/2 oz ",
+    "strMeasure3": "1 oz ",
+    "strMeasure4": null,
+    "strMeasure5": null,
+    "strMeasure6": null,
+    "strMeasure7": null,
+    "strMeasure8": null,
+    "strMeasure9": null,
+    "strMeasure10": null,
+    "strMeasure11": null,
+    "strMeasure12": null,
+    "strMeasure13": null,
+    "strMeasure14": null,
+    "strMeasure15": null,
+    "strImageSource": "https:\/\/commons.wikimedia.org\/wiki\/File:Klassiche_Margarita.jpg",
+    "strImageAttribution": "Cocktailmarler",
+    "strCreativeCommonsConfirmed": "Yes",
+    "dateModified": "2015-08-18 14:42:59"
+  }
